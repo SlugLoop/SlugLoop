cc89d5eba63733db587f73c3b46f9f62d0f3c1e9
NickSzd <nsszwed@gmail.com>
2023-02-04 15:27:00 -0800

    C file with .h files

--- server/aws.c
+++ server/aws.c
@@ -11,8 +11,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include "strlog.h"
 
 #define POST_URL "https://slugloop.azurewebsites.net/"
+
 
 // void error_and_close(const char *err_msg)
 // {
@@ -36,8 +38,6 @@ void aws_post_coordinates(
     if ((h = curl_easy_init()) == NULL) // Creates a handle
     {
         fprintf(stderr, "curl_easy_init() returns NULL\n");
-        //exit(1);
-        // printf("curl_easy_init() returns NULL\n");
         strlog_and_exit("curl_easy_init() returns NULL\n");
     }
 
@@ -48,8 +48,7 @@ void aws_post_coordinates(
         strlen(longitude) > 100)
     {
         printf("Parameter Too Long\n");
-        curl_easy_cleanup(h);
-        //return;
+        curl_easy_cleanup(h); // cleans up and return error
         strlog_and_exit("parameter too long");
     }
 
@@ -57,14 +56,13 @@ void aws_post_coordinates(
             "data=[{\"id\":\"%s\",\"lat\":%s,\"lon\":%s,\"route\":\"%s\"}]",
             bus_id, latitude, longitude, route_name);
 
-    // curl_easy_setopt(h, CURLOPT_URL,            AWS_POST_URL);
+    // curl_easy_setopt(h, CURLOPT_URL,            AWS_POST_URL); // AWS no longer used
     curl_easy_setopt(h, CURLOPT_URL, POST_URL);
     curl_easy_setopt(h, CURLOPT_COPYPOSTFIELDS, buffer);
 
     if ((curl_code = curl_easy_perform(h)) != 0)
     {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curl_code));
-        // printf("curl_easy_perform() returns %d", (int) curl_code);
         strlog("curl_easy_perform() returns %d", (int) curl_code);
     }
 
