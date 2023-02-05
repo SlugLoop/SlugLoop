94f6902ebfb13b8e8081ee0808d12b285b296ca0
Bill Zhang <billzhangsc@gmail.com>
2023-02-04 17:31:18 -0800

    Changes

--- client/src/Components/AboutButton.js
+++ client/src/Components/AboutButton.js
@@ -1,16 +1,59 @@
-import { Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
-import React from "react";
+import { Dialog, DialogTitle, IconButton } from "@mui/material";
+import React, { useState } from "react";
 import HelpOutlineSharpIcon from "@mui/icons-material/HelpOutlineSharp";
-import DialogContentText from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+//import makeStyles from "@mui/styles/makeStyles";
+
+//import ListItemButton from "@mui/material";
 
 export default function AboutButton(props) {
+  const navigate  = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Dialog>
-        <DialogTitle>Submit?</DialogTitle>
-        <DialogContent>Are you sure you want to do this?</DialogContent>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          sx: {
+            position: "absolute",
+            bottom: "30px",
+            left: "20px",
+          },
+        }}
+      >
+        <DialogTitle position="center">Information</DialogTitle>
+        <Button onClick={
+          () => {
+            navigate("/contact");
+            handleClose();
+          }
+        }>Contact Us</Button>
+        <Button
+          onClick={() => {
+            navigate("/about");
+            handleClose();
+          }}
+          autoFocus
+          sx={{
+            paddingBottom: "6%",
+          }}
+        >
+          About Us
+        </Button>
       </Dialog>
       <IconButton
+        onClick={handleClickOpen}
         sx={{
           position: "absolute",
           bottom: "5%",
