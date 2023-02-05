62bd992864fe20a9f93be6ea4e609bfa69607c8d
Annie Liu <aliu98@ucsc.edu>
2023-02-05 08:17:29 -0800

    added themes, fixed multiline messaging

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import { themeOptions } from './Components/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeOptions}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
