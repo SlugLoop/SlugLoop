4441959a42d4be87f5fce81b741e6069aa5c1e50
Bill Zhang <billzhangsc@gmail.com>
2023-02-06 12:23:46 -0800

    Added domain based login and hiding legend table

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,37 @@
 import './App.css';
+import {useEffect} from 'react';
+import {signIn} from './Components/Auth';
 import Map from './Components/Map';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import AboutButton from "./Components/AboutButton";
-
+import AboutButton from './Components/AboutButton';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <><Map /><AboutButton />
-    </>,
+    path: '/',
+    element: (
+      <>
+        <Map />
+        <AboutButton />
+      </>
+    ),
   },
   {
-    path: "/about",
+    path: '/about',
     element: <About />,
   },
   {
-    path: "/contact",
+    path: '/contact',
     element: <Contact />,
   },
 ]);
 
 function App() {
-  return  <RouterProvider router={router} />;
+  useEffect(() => {
+    signIn();
+  }, []);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/Components/Legend.js
+++ client/src/Components/Legend.js
@@ -1,36 +1,42 @@
 import {Box, Stack, Typography} from '@mui/material';
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Legend(props) {
+  const [open, setOpen] = useState(false);
   // Meant to be a legend for the map
   return (
     <Box
       maxWidth="150px"
+      onClick={() => setOpen(!open)}
       sx={{
         padding: 2,
         position: 'absolute',
-        bottom: '150px',
+        bottom: '100px',
         left: '20px',
         backgroundColor: 'white',
         borderRadius: '5px',
         opacity: '0.7',
+
+        // Animation children being rendered
+        transition: 'all 0.5s ease-in-out',
       }}
     >
       <Stack direction="column" spacing={3}>
         <Typography variant="h7">Legend</Typography>
-        {Object.keys(props.legendItems).map((route) => (
-          <Stack direction="row" spacing={2} key={route}>
-            <img
-              src={props.legendItems[route].icon}
-              alt="Bus Icon"
-              width="20px"
-              height="20px"
-            />
-            <Typography variant="body2">
-              {props.legendItems[route].name}
-            </Typography>
-          </Stack>
-        ))}
+        {open &&
+          Object.keys(props.legendItems).map((route) => (
+            <Stack direction="row" spacing={2} key={route}>
+              <img
+                src={props.legendItems[route].icon}
+                alt="Bus Icon"
+                width="20px"
+                height="20px"
+              />
+              <Typography variant="body2">
+                {props.legendItems[route].name}
+              </Typography>
+            </Stack>
+          ))}
       </Stack>
     </Box>
   );
