3ce5a6e705dd685581d7cbc73b2f1b14504b9c57
Bill Zhang <billzhangsc@gmail.com>
2023-02-18 19:30:40 -0800

    Tooltip works

--- client/src/Components/MapMarker.js
+++ client/src/Components/MapMarker.js
@@ -8,17 +8,50 @@ export default function MapMarker(props) {
   Props: bus, heading
   Bus object contains id, lat, lon, route, timestamp
   */
+
+  function convertDateToHumanReadableTime(date) {
+    const currentDateTime = new Date();
+    const myDate = new Date(date);
+    const diffInMilliseconds = currentDateTime.getTime() - myDate.getTime();
+
+    const seconds = Math.floor(diffInMilliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if (seconds < 60) {
+      return `${seconds} seconds ago`;
+    } else if (minutes < 60) {
+      return `${minutes} minutes ago`;
+    } else if (hours < 24) {
+      return `${hours} hours ago`;
+    } else {
+      return `${days} days ago`;
+    }
+  }
   return (
-    <>
+    <Tooltip
+      title={convertDateToHumanReadableTime(props.bus.lastPing)}
+      placement="top"
+      arrow
+    >
       <Box
-        component="img"
-        src={`1.ico`}
         sx={{
-          //Rotate the marker based on the heading of the bus in radians
-          transform: `rotate(${props.heading}deg)`,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
-      />
-      <Typography>{props.bus.id}</Typography>
-    </>
+      >
+        <Box
+          component="img"
+          src={`1.ico`}
+          sx={{
+            //Rotate the marker based on the heading of the bus in radians
+            transform: `rotate(${props.heading}deg)`,
+          }}
+        />
+        <Typography>{props.bus.id}</Typography>
+      </Box>
+    </Tooltip>
   );
 }
