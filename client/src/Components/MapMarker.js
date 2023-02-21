44a8b4ad6c0e79e49e4b6138e0976174b8c6d248
Bill Zhang <billzhangsc@gmail.com>
2023-02-21 15:30:49 -0800

    Added icons

--- client/src/Components/MapMarker.js
+++ client/src/Components/MapMarker.js
@@ -12,13 +12,12 @@ export default function MapMarker(props) {
     <>
       <Box
         component="img"
-        src={`1.ico`}
+        src={`${props.color}.ico`}
         sx={{
           //Rotate the marker based on the heading of the bus in radians
           transform: `rotate(${props.heading}deg)`,
         }}
       />
-      <Typography>{props.bus.id}</Typography>
     </>
   );
 }
