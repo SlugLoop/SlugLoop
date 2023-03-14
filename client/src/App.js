ec6924e73bfd5b4c5a6de1b54a17e5967c998b80
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-03-13 21:16:06 -0700

    Metro page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import {useEffect} from 'react';
-import {signIn} from './Components/Auth';
-import Map from './Components/Map';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import AboutButton from './Components/AboutButton';
+import './App.css'
+import {useEffect} from 'react'
+import {signIn} from './Components/Auth'
+import Map from './Components/Map'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import AboutButton from './Components/AboutButton'
+import Metro from './Components/Metro'
 
 const router = createBrowserRouter([
   {
@@ -25,13 +26,17 @@ const router = createBrowserRouter([
     path: '/contact',
     element: <Contact />,
   },
-]);
+  {
+    path: '/metro',
+    element: <Metro />,
+  },
+])
 
 function App() {
   useEffect(() => {
-    signIn();
-  }, []);
-  return <RouterProvider router={router} />;
+    signIn()
+  }, [])
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ client/src/Components/Metro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Metro() {
+  return (
+    <iframe
+      src="https://cruzmetro.com/map#mainContent"
+      title="Metro"
+      width="100%"
+      height="100%"
+    />
+  )
+}
