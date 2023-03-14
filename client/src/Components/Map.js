4dc40a61019da844552ff83d5176e16a3776dfc2
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-03-13 22:08:39 -0700

    First draft

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import MapComponent from './MapComponent';
+import React, {useState} from 'react'
+import MapComponent from './MapComponent'
+import Metro from './Metro'
+import Button from '@mui/material/Button'
 
 export default function Map() {
   // Coordinates of UCSC
-  const center = {lat: 36.99, lng: -122.06};
-  const zoom = 15;
+  const center = {lat: 36.99, lng: -122.06}
+  const zoom = 15
+  const [displayUCSC, setDisplayUCSC] = useState(true)
 
-  return <MapComponent center={center} zoom={zoom} />;
+  function toggleDisplayUCSC() {
+    setDisplayUCSC(!displayUCSC)
+  }
+
+  return (
+    <>
+      {displayUCSC ? <MapComponent center={center} zoom={zoom} /> : <Metro />}
+
+      <Button
+        onClick={toggleDisplayUCSC}
+        disableRipple
+        sx={{
+          position: 'absolute',
+          top: '20px',
+          left: '50px',
+          scale: '1.5',
+
+          backgroundColor: 'white',
+          borderRadius: '5px',
+          opacity: '0.7',
+        }}
+      >
+        {displayUCSC ? 'Display Metro Buses' : 'Display Loop Buses'}
+      </Button>
+    </>
+  )
 }
--- client/src/Components/Metro.js
+++ client/src/Components/Metro.js
@@ -1,12 +1,37 @@
+import {Box} from '@mui/material'
 import React from 'react'
+import AboutButton from './AboutButton'
 
 export default function Metro() {
+  const height = window.innerHeight + 175
   return (
-    <iframe
-      src="https://cruzmetro.com/map#mainContent"
-      title="Metro"
-      width="100%"
-      height="100%"
-    />
+    <Box
+      width="100vw"
+      height={window.innerHeight}
+      sx={{
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        bottom: '0px',
+        right: '0px',
+        overflow: 'hidden',
+      }}
+    >
+      <iframe
+        src="https://cruzmetro.com/map#map_content"
+        title="Metro"
+        width="100%"
+        height={height}
+        style={{
+          position: 'relative',
+          top: '-177px',
+          left: '0px',
+          bottom: '0px',
+          right: '0px',
+          overflow: 'hidden',
+        }}
+      />
+      <AboutButton />
+    </Box>
   )
 }
