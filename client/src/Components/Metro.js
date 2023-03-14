f7edba349b4045fee2ee6847a880a87967126a68
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-03-13 23:29:27 -0700

    UI Update

--- client/src/Components/Metro.js
+++ client/src/Components/Metro.js
@@ -3,7 +3,16 @@ import React from 'react'
 import AboutButton from './AboutButton'
 
 export default function Metro() {
-  const height = window.innerHeight + 175
+  let offset
+  if (window.innerWidth > 600) {
+    // DO NOT TOUCH THIS CODE
+    offset = 140
+  } else if (window.innerWidth <= 400) {
+    offset = 210
+  } else {
+    offset = 180
+  }
+  const height = window.innerHeight + offset
   return (
     <Box
       width="100vw"
@@ -24,7 +33,7 @@ export default function Metro() {
         height={height}
         style={{
           position: 'relative',
-          top: '-177px',
+          top: `-${offset}px`,
           left: '0px',
           bottom: '0px',
           right: '0px',
