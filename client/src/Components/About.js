3e358edd77ec46ccf34d20dda9315eee48332244
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-03-14 09:56:10 -0700

    Completed

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,96 +1,134 @@
-import React from 'react';
-import {Box, Button, ListItem, List} from '@mui/material';
-import {Typography} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
+import React from 'react'
+import {Box, Button, List, Stack} from '@mui/material'
+import {Typography} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
+import {motion} from 'framer-motion'
 
-export default function About() {
-  const navigate = useNavigate();
+const contributors = [
+  'Bill Zhang (billzhangsc@gmail.com)',
+  'Annie Liu (aliu98@ucsc.edu)',
+  'Alex Liu (liu.alex01@gmail.com)',
+  'Nick Szwed (nsszwed@gmail.com)',
+]
+
+const organizations = [
+  'Professor Kerry Veenstra',
+  'Santa Cruz Metro',
+  'UCSC TAPS',
+  'Past Contributors of BTS 2 and 3',
+]
+
+const About = () => {
+  const navigate = useNavigate()
+
+  const containerVariant = {
+    hidden: {opacity: 0},
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0,
+        staggerChildren: 0.05,
+      },
+    },
+  }
+
+  const itemVariant = {
+    hidden: {opacity: 0, y: -20},
+    visible: {opacity: 1, y: 0},
+  }
+
   return (
-    <div>
+    <motion.div initial="hidden" animate="visible" variants={containerVariant}>
       <Box
         display="flex"
         width="100vw"
         height="100vh"
-        component="form"
         sx={{
           alignItems: 'center',
-          justifyContent: 'center',
+          justifyContent: 'flex-start',
           flexDirection: 'column',
         }}
-        noValidate
-        autoComplete="off"
       >
-        <Box
-          justifyContent={'left'}
-          paddingLeft={'10vh'}
-          paddingRight={'10vh'}
-          paddingTop={'1vh'}
+        <Stack
+          maxWidth="500px"
+          paddingLeft={'8vw'}
+          paddingRight={'8vw'}
+          paddingTop={'8vh'}
+          alignItems="flex-start"
+          spacing={2}
         >
-          <Typography variant="h4" component="div" paddingBottom={'2vh'}>
-            About
-          </Typography>
+          <motion.div variants={itemVariant}>
+            <Typography variant="h3" component="div">
+              About Us
+            </Typography>
+          </motion.div>
 
-          <Typography variant="h5" component="div" fontWeight={'bold'}>
-            What is Slug Loop?
-          </Typography>
-
-          <Typography component="div" align="left" paddingBottom={'3vh'}>
-            Slug Loop provides UCSC campus bus locations in real time.
-          </Typography>
-
-          <Typography variant="h5" component="div" fontWeight={'bold'}>
-            Who worked on Slug Loop?
-          </Typography>
-
-          <Typography>
-            <List
-              sx={{
-                listStyleType: 'disc',
-                pl: 2,
-                '& .MuiListItem-root': {
-                  display: 'list-item',
-                },
-              }}
-            >
-              <ListItem>Bill Zhang (billzhangsc@gmail.com)</ListItem>
-              <ListItem>Annie Liu (aliu98@ucsc.edu)</ListItem>
-              <ListItem>Alex Liu (liu.alex01@gmail.com)</ListItem>
-              <ListItem>Nick Szwed (nsszwed@gmail.com) </ListItem>
-            </List>
-          </Typography>
-
-          <Typography
-            variant="h5"
-            align="left"
-            paddingBottom={'1.5vh'}
-            fontWeight={'bold'}
+          <motion.div variants={itemVariant}>
+            <Typography variant="h5" component="div" fontWeight={'bold'}>
+              Project Goals:
+            </Typography>
+          </motion.div>
+          <motion.div variants={itemVariant}>
+            <Typography component="div">
+              Slug Loop provides UCSC campus bus locations in real time.
+            </Typography>
+          </motion.div>
+          <motion.div variants={itemVariant}>
+            <Typography variant="h5" fontWeight={'bold'}>
+              Contributors:
+            </Typography>
+          </motion.div>
+          <List
+            dense
+            sx={{
+              padding: 0,
+            }}
           >
-            Our project stands on the shoulders of giants.
-          </Typography>
-          <Typography>
-            Many thanks to the people who inspired us, including:
-            <List
-              sx={{
-                listStyleType: 'disc',
-                pl: 2,
-                '& .MuiListItem-root': {
-                  display: 'list-item',
-                },
-              }}
-            >
-              <ListItem>Professor Veenstra</ListItem>
-              <ListItem>Past contributors of BTS 2.0</ListItem>
-            </List>
-          </Typography>
-        </Box>
-        <Button
-          onClick={() => {
-            navigate('/');
-          }}
-        >
-          Back to Map
-        </Button>
+            {contributors.map((contributor, index) => (
+              <motion.li key={index} variants={itemVariant}>
+                {`• ${contributor}`}
+              </motion.li>
+            ))}
+          </List>
+          <motion.div variants={itemVariant}>
+            <Typography variant="h5" align="left" fontWeight={'bold'}>
+              Our project stands on the shoulders of giants.
+            </Typography>
+          </motion.div>
+          <motion.div variants={itemVariant}>
+            <Typography>
+              Many thanks to the people and organizations who inspired us,
+              including:
+            </Typography>
+          </motion.div>
+          <List
+            dense
+            sx={{
+              padding: 0,
+            }}
+          >
+            {organizations.map((organization, index) => (
+              <motion.li key={index} variants={itemVariant}>
+                {`• ${organization}`}
+              </motion.li>
+            ))}
+          </List>
+        </Stack>
+        <motion.div variants={itemVariant}>
+          <Button
+            onClick={() => {
+              navigate('/')
+            }}
+            sx={{
+              mt: '5vh',
+            }}
+          >
+            Back to Map
+          </Button>
+        </motion.div>
       </Box>
-    </div>
-  );
+    </motion.div>
+  )
 }
+
+export default About
