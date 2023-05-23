dd29484b295967e631b4fe87292005c3dc257001
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-05-22 21:43:09 -0700

    Works

--- client/src/Components/RouteSelector.js
+++ client/src/Components/RouteSelector.js
@@ -23,7 +23,7 @@ const loopRoutes = [
   'SPECIAL',
 ]
 
-export default function RouteSelector(props) {
+export default function RouteSelector() {
   const [selectedRoute, setSelectedRoute] = useContext(RouteContext)
   const [open, setOpen] = useState('')
   const [isDrawerOpen, setDrawerOpen] = useState(false)
@@ -75,7 +75,7 @@ export default function RouteSelector(props) {
   )
 
   const CustomListItem = (value) => (
-    <ListItem button onClick={() => handleToggle(value)}>
+    <ListItem onClick={() => handleToggle(value)}>
       <ListItemText primary={value} />
       {open === value ? <ExpandLess /> : <ExpandMore />}
     </ListItem>
@@ -99,11 +99,93 @@ export default function RouteSelector(props) {
         <MenuIcon />
       </IconButton>
       <Drawer anchor="right" open={isDrawerOpen} onClose={handleDrawerClose}>
-        <List>
-          {CustomListItem('Loop')}
-          {RouteSelectorList(loopRoutes, 'Loop')}
-          {CustomListItem('Metro')}
-          {RouteSelectorList(metroRoutes, 'Metro')}
+        <List
+          sx={{
+            width: '300px',
+          }}
+        >
+          <ListItem
+            onClick={() => {
+              if (open === 'Loop') {
+                setOpen('')
+              } else {
+                setOpen('Loop')
+              }
+            }}
+          >
+            <ListItemText primary="Loop" />
+            {open === 'Loop' ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open === 'Loop'} timeout="auto" unmountOnExit>
+            {loopRoutes.map((route) => (
+              <ListItemButton
+                key={route}
+                onClick={() => {
+                  if (selectedRoute.includes(route)) {
+                    setSelectedRoute(selectedRoute.filter((r) => r !== route))
+                  } else {
+                    setSelectedRoute([...selectedRoute, route])
+                  }
+                }}
+                sx={{
+                  padding: 0,
+                  paddingTop: '5px',
+                  paddingBottom: '5px',
+                }}
+              >
+                <Checkbox checked={selectedRoute.includes(route)} />
+                <ListItemText primary={route} />
+                <ListItemIcon>
+                  <img
+                    src={routeColors[route]}
+                    alt="bus"
+                    style={{width: '20px', height: '20px'}}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            ))}
+          </Collapse>
+          <ListItem
+            onClick={() => {
+              if (open === 'Metro') {
+                setOpen('')
+              } else {
+                setOpen('Metro')
+              }
+            }}
+          >
+            <ListItemText primary="Metro" />
+            {open === 'Metro' ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open === 'Metro'} timeout="auto" unmountOnExit>
+            {metroRoutes.map((route) => (
+              <ListItemButton
+                key={route}
+                onClick={() => {
+                  if (selectedRoute.includes(route)) {
+                    setSelectedRoute(selectedRoute.filter((r) => r !== route))
+                  } else {
+                    setSelectedRoute([...selectedRoute, route])
+                  }
+                }}
+                sx={{
+                  padding: 0,
+                  paddingTop: '5px',
+                  paddingBottom: '5px',
+                }}
+              >
+                <Checkbox checked={selectedRoute.includes(route)} />
+                <ListItemText primary={route} />
+                <ListItemIcon>
+                  <img
+                    src={routeColors[route]}
+                    alt="bus"
+                    style={{width: '20px', height: '20px'}}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            ))}
+          </Collapse>
           <ListItemButton
             onClick={() => {
               setSelectedRoute([])
