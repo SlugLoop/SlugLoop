21b6797d4bb8a2aa54786e46f62cb3cb96bed93c
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-05-26 01:03:06 -0700

    Added service workers

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {signIn} from './Components/Auth'
 import Map from './Components/Map'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
@@ -9,54 +9,66 @@ import Main from './Components/Landing/Main'
 import MyTimeline from './Components/TimeLine/TimeLine'
 import Wrapper from './Components/UIWrapper/Wrapper'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Wrapper>
-        <Main />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/timeline',
-    element: (
-      <Wrapper>
-        <MyTimeline />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/map',
-    element: <Map />,
-  },
-  {
-    path: '/about',
-    element: (
-      <Wrapper>
-        <About />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/contact',
-    element: (
-      <Wrapper>
-        <Contact />
-      </Wrapper>
-    ),
-  },
-])
-
 function App() {
+  const viewportWidth = useViewportWidth()
   useEffect(() => {
     signIn()
   }, [])
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <Wrapper>
+          <Main />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/timeline',
+      element: (
+        <Wrapper>
+          <MyTimeline />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/map',
+      element: <Map />,
+    },
+    {
+      path: '/about',
+      element: (
+        <Wrapper>
+          <About />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/contact',
+      element: (
+        <Wrapper>
+          <Contact />
+        </Wrapper>
+      ),
+    },
+  ])
   return (
     <RouteProvider>
       <RouterProvider router={router} />
     </RouteProvider>
   )
+}
+const useViewportWidth = () => {
+  const [viewportWidth, setViewportWidth] = useState(null)
+
+  useEffect(() => {
+    const handleResize = () => setViewportWidth(window.innerWidth)
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return viewportWidth
 }
 
 export default App
