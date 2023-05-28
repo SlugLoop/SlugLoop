4c6f3ee4746b08b68ea52b2749f84315b3943319
igpang <123428889+igpang@users.noreply.github.com>
2023-05-27 17:46:35 -0700

    Update index.js
    Added documentation for what the functions are doing.

--- server/routes/index.js
+++ server/routes/index.js
@@ -315,31 +315,39 @@ function calcCWorCCW({lat1, lon1}, previousLocationArray) {
 function nextBusStops({lat1, lon1}, previousLocationArray) {
   let direction = calcCWorCCW({lat1, lon1}, previousLocationArray)
 
+  // If heading of bus is Clock-wise
   if (direction == "cw") {
     const cwData = busStops.bstop.CW;
+    // Iterates through all CW bus stops
     for (let i = 0; i < cwData.length; i++) {
       let location = cwData[i];
       let locationName = Object.keys(location)[0];
       let lat2 = location[locationName].lat;
       let lon2 = location[locationName].lon;
+      // Find the bus stop closest to the bus, and store next 3 stops into an array
       if((lat2 - 0.000450) <= lat1 <= (lat2 + 0.000450)) {
         if ((lon2 - 0.000450) <= lon1 <= (lon2 + 0.000450)) {
           let stops_arr = [Object.keys(cwData[i + 1])[0], Object.keys(cwData[i + 2])[0], Object.keys(cwData[i + 3])[0]]
+          // Pass the array into function for database updates for CW bus stops
           soonBusStopUpdate(stops_arr, "CW");
         }
       } 
     }
   } 
+  // If heading of bus is Counter Clock-wise
   else if(direction == "ccw") {
     const ccwData = busStops.bstop.CCW;
+    // Iterates through all CCW bus stops
     for (let i = 0; i < ccwData.length; i++) {
       let location = ccwData[i];
       let locationName = Object.keys(location)[0];
       let lat2 = location[locationName].lat;
       let lon2 = location[locationName].lon;
+      // Find the bus stop closest to the bus, and store next 3 stops into an array
       if((lat2 - 0.000450) <= lat1 <= (lat2 + 0.000450)) {
         if ((lon2 - 0.000450) <= lon1 <= (lon2 + 0.000450)) {
           let stops_arr = [Object.keys(ccwData[i + 1])[0], Object.keys(ccwData[i + 2])[0], Object.keys(ccwData[i + 3])[0]]
+          // Pass the array into function for database updates for CCW bus stops
           soonBusStopUpdate(stops_arr, "CCW");
         }
       }
@@ -347,8 +355,7 @@ function nextBusStops({lat1, lon1}, previousLocationArray) {
   }
 }
 
-// Set bus stops "Soon" to true if 
-
+// Set bus stops "Soon" to true in the firestore DB if in array
 function soonBusStopUpdate(soonBusStops, direction) {
   // Get a database reference to the collection of stops given a direction
   const stopRef = defaultDatabase.collection('busStop').document(direction);
@@ -356,11 +363,17 @@ function soonBusStopUpdate(soonBusStops, direction) {
   // Get all the collection values inside the doc
   const stops =  async () => {
     await stopRef.listCollections();
+    
+    // Iterate through each collection
     stops.forEach(stop => {
+      
+      // If bus stop is in array, set soon = true
       if(soonBusStops.includes(stop.id)) {
         stop.update({
           soon: true
         });
+        
+      // Otherwise update soon = false
       } else {
         stop.update({
           soon: false
