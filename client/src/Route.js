8f0e9cb7a08f36f040a146d196e754e0bb16d298
Ruthwika Gajjala <rgajjala@ucsc.edu>
2024-02-21 13:12:25 -0800

    updated frontend documentation

--- client/src/Route.js
+++ client/src/Route.js
@@ -1,8 +1,13 @@
-import {createContext, useState} from 'react'
+import {createContext, useState} from 'react' // imports createContext and useState functions from the react library
 
-export const RouteContext = createContext()
-
-export const RouteProvider = ({children}) => {
+export const RouteContext = createContext() // creates a new context object RouteContext
+/**
+ * RouteProvider is a function that takes any children components as a parameter and encapsulates the the state related to routes,
+ * making it available to any child component that needs access to the data 
+ * @param children - any potential descendants of Route Provider
+ * @returns value - provides the selected Route state and updates it using the setSelectedRoute
+ */
+export const RouteProvider = ({children}) => { // defines a function Route Provider
   const [selectedRoute, setSelectedRoute] = useState([
     'LOOP',
     'UPPER CAMPUS',
@@ -10,11 +15,11 @@ export const RouteProvider = ({children}) => {
     'EAST NIGHT CORE',
     'OUT OF SERVICE/SORRY',
     'SPECIAL',
-  ])
+  ]) // initalizes a state variable selectedRoute using the useState hook
 
   return (
     <RouteContext.Provider value={[selectedRoute, setSelectedRoute]}>
       {children}
     </RouteContext.Provider>
-  )
+  ) // returns the RouteContext.Provider component, which is a an array of selectedRoute and the updater function setSelectedRoute
 }
