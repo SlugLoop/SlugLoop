c7b63607933091829d3dafc5e46bbc54c5464c3c
Kevin Yosifov <kyosifov@ucsc,edu>
2024-04-30 22:18:06 -0700

    Displaying the bus stops on the frontend (And the ETAs)

--- client/src/Components/StopMarkers.js
+++ client/src/Components/StopMarkers.js
@@ -14,7 +14,23 @@ export default function StopMarker(props) {
   Stop object contains id, lat, lon, name
   */
 
-  console.log(props.eta)
+  // I'm converting the names into more human readable names because it's nice for users
+  const humanReadableName = (name) => {
+    let returnedName = ""
+    for (let i = 0; i < name.length; i++) {
+      if (name[i] === "_") {
+        returnedName += " & "
+      } else {
+        if (name[i] === name[i].toUpperCase() && i !== 0 && isNaN(name[i])) {
+          returnedName += " "
+        }
+        returnedName += name[i]
+      }
+    }
+    return returnedName
+  }
+
+  const displayName = humanReadableName(props.name)
 
   function formatTime(time) {
     if (time < 60) {
@@ -28,12 +44,18 @@ export default function StopMarker(props) {
     <Tooltip title=
       {
         <>
-          <Box component="div">{props.name}</Box>
-          {props.eta !== null && <Box component="div">{formatTime(props.eta)}</Box>}
+          <Box component="div" style={{fontSize:"1rem"}}>{displayName}</Box>
+          {props.eta !== null && <Box component="div" style={{fontSize:"0.8rem"}}>{formatTime(props.eta)}</Box>}
         </>
       }
-     placement="top">
-      <Box component="img" src={busStop} alt={props.name} height="1rem" minHeight="20px" width="1rem" minWidth="20px"/>
+     placement="top" style={{position:"relative"}}>
+      <Box component="img" src={busStop} alt={displayName} style={{
+        position:"relative",
+        transform: 'translate(-50%, -50%)',
+        height:"1rem",
+        minHeight:"20px",
+        width:"1rem",
+        minWidth:"20px"}}/>
     </Tooltip>
   )
 }
